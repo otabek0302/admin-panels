9e30ebcd13ccc37039d9f6bc10ac8067bc4d40d2
Otabek <otabekjon2002@mail.ru>
2025-03-24 02:06:19 +0500

    updated: React Admin - Shadcn Ui: header is done

--- React Admin - Shadcn Ui/src/components/sections/Header.tsx
+++ React Admin - Shadcn Ui/src/components/sections/Header.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+"use client";
+
+import { SearchForm } from "@/components/ui/search-form";
+import { Button } from "../ui/button";
+import { Bell, Maximize, Minimize, Moon, Sun } from "lucide-react";
+import { LanguagesDropdown } from "../ui/languages-dropdown";
+import { useTheme } from "next-themes";
+import { useState } from "react";
+import { NavUser } from "../ui/nav-user";
 
 const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { theme, setTheme } = useTheme();
+  const [isMaximized, setIsMaximized] = useState(false);
 
-export default Header
+  const handleMaximize = () => {
+    setIsMaximized(!isMaximized);
+    if (isMaximized) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
+    }
+  };
+
+  return (
+    <header className="px-4 py-3 flex items-center justify-between border-b bg-background">
+      <div className="w-96">
+        <SearchForm />
+      </div>
+      <div className="flex-1 flex items-center justify-end gap-2">
+        <LanguagesDropdown />
+        <Button variant="outline" size="icon" iconSize="md" className="shadow-none cursor-pointer" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+          {theme === "dark" ? <Sun className="text-primary" /> : <Moon className="text-primary" />}
+        </Button>
+        <Button variant="outline" size="icon" iconSize="md" className="shadow-none cursor-pointer">
+          <Bell className="text-primary" />
+        </Button>
+        <Button variant="outline" size="icon" iconSize="md" className="shadow-none cursor-pointer" onClick={handleMaximize}>
+          {isMaximized ? <Minimize className="text-primary" /> : <Maximize className="text-primary" />}
+        </Button>
+        <NavUser />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- React Admin - Shadcn Ui/src/components/sections/Sidebar.tsx
+++ React Admin - Shadcn Ui/src/components/sections/Sidebar.tsx
@@ -1,107 +1,107 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 import { Logo } from "@/assets";
-import { Activity, Bell, ChartBar, HomeIcon, LogOut, Package, Settings, Truck, User, Users } from 'lucide-react';
-import { Button } from '../ui/button';
-
+import { Activity, Bell, ChartBar, HomeIcon, LogOut, Package, Settings, Truck, User, Users } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const Sidebar = () => {
   return (
-    <aside className='flex h-screen w-64 flex-col justify-start overflow-y-auto border-r bg-background px-4 py-6'>
+    <aside className="flex h-screen w-64 flex-col justify-start overflow-y-auto border-r bg-background px-4">
       {/* Logo */}
-      <div className='flex flex-col items-center gap-10 border-b'>
-        <Link href='/'>
-          <Image src={Logo} alt="logo" width={100} height={100} />
+      <div className="flex items-center justify-center gap-10 border-b">
+        <Link href="/">
+          <Image src={Logo} alt="logo" className="w-16 h-16" />
         </Link>
       </div>
+
       {/* Sidebar */}
-      <div className='flex flex-col gap-8 px-2 py-4'>
-        <div className='flex flex-col gap-2'>
-            <h2 className='text-sm font-bold text-primary uppercase'>Main</h2>
-            <Button variant="ghost" iconSize="md" className='w-full justify-start gap-4' asChild>
-              <Link href="/">
-                <HomeIcon className='h-6 w-6 text-primary' />
-                <span>Home</span>
-              </Link>
-            </Button>
+      <div className="flex flex-col gap-8 px-2 py-4">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-sm font-bold text-primary uppercase">Main</h2>
+          <Button variant="ghost" iconSize="md" className="w-full justify-start gap-4" asChild>
+            <Link href="/">
+              <HomeIcon className="h-6 w-6 text-primary" />
+              <span>Home</span>
+            </Link>
+          </Button>
         </div>
-        <div className='flex flex-col gap-2'>
-            <h2 className='text-sm font-bold text-primary uppercase'>Lists</h2>
-            <Button variant="ghost" iconSize="md" className='w-full justify-start gap-4' asChild>
-              <Link href="/users">
-                <Users className='text-primary' />
-                <span>Users</span>
-              </Link>
-            </Button>
-            <Button variant="ghost" iconSize="md" className='w-full justify-start gap-4' asChild>
-              <Link href="/products">
-                <Package className='text-primary' />
-                <span>Products</span>
-              </Link>
-            </Button>
-            <Button variant="ghost" iconSize="md" className='w-full justify-start gap-4' asChild>
-              <Link href="/orders">
-                <Package className='text-primary' />
-                <span>Orders</span>
-              </Link>
-            </Button>
-            <Button variant="ghost" iconSize="md" className='w-full justify-start gap-4' asChild>
-              <Link href="/delivery">
-                <Truck className='text-primary' />
-                <span>Delivery</span>
-              </Link>
-            </Button>
+        <div className="flex flex-col gap-2">
+          <h2 className="text-sm font-bold text-primary uppercase">Lists</h2>
+          <Button variant="ghost" iconSize="md" className="w-full justify-start gap-4" asChild>
+            <Link href="/users">
+              <Users className="text-primary" />
+              <span>Users</span>
+            </Link>
+          </Button>
+          <Button variant="ghost" iconSize="md" className="w-full justify-start gap-4" asChild>
+            <Link href="/products">
+              <Package className="text-primary" />
+              <span>Products</span>
+            </Link>
+          </Button>
+          <Button variant="ghost" iconSize="md" className="w-full justify-start gap-4" asChild>
+            <Link href="/orders">
+              <Package className="text-primary" />
+              <span>Orders</span>
+            </Link>
+          </Button>
+          <Button variant="ghost" iconSize="md" className="w-full justify-start gap-4" asChild>
+            <Link href="/delivery">
+              <Truck className="text-primary" />
+              <span>Delivery</span>
+            </Link>
+          </Button>
         </div>
-        <div className='flex flex-col gap-2'>
-            <h2 className='text-sm font-bold text-primary uppercase'>Useful</h2>
-            <Button variant="ghost" iconSize="md" className='w-full justify-start gap-4' asChild>
-              <Link href="/stats">
-                <ChartBar className='text-primary' />
-                <span>Stats</span>
-              </Link>
-            </Button>
-            <Button variant="ghost" iconSize="md" className='w-full justify-start gap-4' asChild>
-              <Link href="/notifications">
-                <Bell className='text-primary' />
-                <span>Notifications</span>
-              </Link>
-            </Button>
+        <div className="flex flex-col gap-2">
+          <h2 className="text-sm font-bold text-primary uppercase">Useful</h2>
+          <Button variant="ghost" iconSize="md" className="w-full justify-start gap-4" asChild>
+            <Link href="/stats">
+              <ChartBar className="text-primary" />
+              <span>Stats</span>
+            </Link>
+          </Button>
+          <Button variant="ghost" iconSize="md" className="w-full justify-start gap-4" asChild>
+            <Link href="/notifications">
+              <Bell className="text-primary" />
+              <span>Notifications</span>
+            </Link>
+          </Button>
         </div>
-        <div className='flex flex-col gap-2'>
-            <h2 className='text-sm font-bold text-primary uppercase'>Services</h2>
-            <Button variant="ghost" iconSize="md" className='w-full justify-start gap-4' asChild>
-              <Link href="/activity">
-                <Activity className='text-primary' />
-                <span>Activity</span>
-              </Link>
-            </Button>
-            <Button variant="ghost" iconSize="md" className='w-full justify-start gap-4' asChild>
-              <Link href="/settings">
-                <Settings className='text-primary' />
-                <span>Settings</span>
-              </Link>
-            </Button>
+        <div className="flex flex-col gap-2">
+          <h2 className="text-sm font-bold text-primary uppercase">Services</h2>
+          <Button variant="ghost" iconSize="md" className="w-full justify-start gap-4" asChild>
+            <Link href="/activity">
+              <Activity className="text-primary" />
+              <span>Activity</span>
+            </Link>
+          </Button>
+          <Button variant="ghost" iconSize="md" className="w-full justify-start gap-4" asChild>
+            <Link href="/settings">
+              <Settings className="text-primary" />
+              <span>Settings</span>
+            </Link>
+          </Button>
         </div>
-        <div className='flex flex-col gap-2'>
-            <h2 className='text-sm font-bold text-primary uppercase'>Profile</h2>
-            <Button variant="ghost" iconSize="md" className='w-full justify-start gap-4' asChild>
-              <Link href="/profile">
-                <User className='text-primary' />
-                <span>Profile</span>
-              </Link>
-            </Button>
-            <Button variant="ghost" iconSize="md" className='w-full justify-start gap-4' asChild>
-              <Link href="/logout">
-                <LogOut className='text-primary' />
-                <span>Logout</span>
-              </Link>
-            </Button>
+        <div className="flex flex-col gap-2">
+          <h2 className="text-sm font-bold text-primary uppercase">Profile</h2>
+          <Button variant="ghost" iconSize="md" className="w-full justify-start gap-4" asChild>
+            <Link href="/profile">
+              <User className="text-primary" />
+              <span>Profile</span>
+            </Link>
+          </Button>
+          <Button variant="ghost" iconSize="md" className="w-full justify-start gap-4" asChild>
+            <Link href="/logout">
+              <LogOut className="text-primary" />
+              <span>Logout</span>
+            </Link>
+          </Button>
         </div>
       </div>
     </aside>
   );
 };
 
-export default Sidebar
+export default Sidebar;
