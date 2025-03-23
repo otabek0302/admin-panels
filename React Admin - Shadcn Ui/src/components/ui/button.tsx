53be8bc3485e995e9d33d0fdc165a1554b34a28d
Otabek <otabekjon2002@mail.ru>
2025-03-24 01:02:02 +0500

    updated: React Admin - Shadcn Ui: sidebar is ready, custom theme is inserted

--- React Admin - Shadcn Ui/src/components/ui/button.tsx
+++ React Admin - Shadcn Ui/src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
@@ -17,8 +17,7 @@ const buttonVariants = cva(
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+        ghost: "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
@@ -27,31 +26,40 @@ const buttonVariants = cva(
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
+      iconSize: {
+        default: "[&_svg]:size-4",
+        sm: "[&_svg]:size-3",
+        md: "[&_svg]:size-5",
+        lg: "[&_svg]:size-6",
+        xl: "[&_svg]:size-7",
+      },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
+      iconSize: "default",
     },
   }
 )
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: React.ComponentProps<"button"> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
+function Button({ 
+  className, 
+  variant, 
+  size, 
+  iconSize,
+  asChild = false, 
+  ...props 
+}: React.ComponentProps<"button"> & 
+   VariantProps<typeof buttonVariants> & 
+   { asChild?: boolean }
+) {
   const Comp = asChild ? Slot : "button"
 
   return (
-    <Comp
-      data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
+    <Comp 
+      data-slot="button" 
+      className={cn(buttonVariants({ variant, size, iconSize, className }))} 
+      {...props} 
     />
   )
 }
