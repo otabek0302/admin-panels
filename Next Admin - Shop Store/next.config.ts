1f9a7d88ef888e173f69089bd1b071afe7215eb0
Otabek <otabekjon2002@mail.ru>
2025-05-21 06:56:34 +0500

    fix: downgrade mongodb to v5.7.0 for NextAuth compatibility

--- Next Admin - Shop Store/next.config.ts
+++ Next Admin - Shop Store/next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
+import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: true, // or anything else you're using
+  },
+  webpack: (config: Configuration) => {
+    config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
+    return config;
+  },
   images: {
     domains: ['res.cloudinary.com'],
   },
