d42b23e3290d6bf208758e8c54922beb11800fe7
Otabek <otabekjon2002@mail.ru>
2025-05-22 14:23:23 +0500

    Reinstalled Libraries

--- Next Admin - Shop Store/next.config.ts
+++ Next Admin - Shop Store/next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
-import path from "path";
-import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: {}, // ✅ must be an object
-  },
-  webpack: (config: Configuration) => {
-    config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
-    return config;
   },
   images: {
     domains: ['res.cloudinary.com'],
